c6edf5e01a4640cfb9f8c8f6117c8dcfc1004eb7
Abdulrahman84 <55898084+Abdulrahman84@users.noreply.github.com>
2021-08-03 22:56:36 +0300

    test

--- app.js
+++ app.js
@@ -1,48 +1,54 @@
+const path = require("path");
 require("dotenv").config();
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// const socketio = require("socket.io");
-// const http = require("http");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-// const userRouter = require("./routes/user");
-// const postRouter = require("./routes/post");
-// const followRouter = require("./routes/follow");
-// const commentRouter = require("./routes/comment");
+const userRouter = require("./routes/user");
+const postRouter = require("./routes/post");
+const followRouter = require("./routes/follow");
+const commentRouter = require("./routes/comment");
 
-// const app = express();
+const app = express();
 const port = process.env.PORT || 3000;
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-
-// app.use(userRouter);
-// app.use(postRouter);
-// app.use(followRouter);
-// app.use(commentRouter);
-
-// app.get("/", (req, res) => res.send("Hi There"));
-
-// const server = http.createServer(app);
-
-// server.listen(port);
-// mongoose.connect(
-//   process.env.MONGODB_URI,
-//   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
-//   (err, res) => {
-//     // app.set("socketIo", io);
-//     console.log("connected " + port);
-//   }
-// );
-
-const app = require("express")();
-const httpServer = require("http").createServer(app);
+const server = require("http").createServer(app);
 const options = { cors: { origin: "*" } };
-const io = require("socket.io")(httpServer, options);
+const io = require("socket.io")(server, options);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(userRouter);
+app.use(postRouter);
+app.use(followRouter);
+app.use(commentRouter);
+
+app.get("/", (req, res) => res.send("Hi There"));
+
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
+  (err, res) => {
+    server.listen(port);
+    console.log("connected " + port);
+  }
+);
 
 io.on("connection", (socket) => {
   console.log("new WS");
   socket.emit("test", { welcome: "hello from server", name: "abdulrahman" });
+  socket.on("addComment", (data) => {
+    socket.emit("addComment", data);
+  });
 });
-httpServer.listen(port);
+
+// io.sockets.on("connection", function (socket) {
+//   console.log("client connect");
+//   socket.on("echo", function (data) {
+//     io.sockets.emit("message", data);
+//   });
+// });
+
+require("./routes/comment")(app, io);
--- routes/comment.js
+++ routes/comment.js
@@ -6,11 +6,22 @@ const commentController = require("../controllers/commentController");
 
 const router = express.Router();
 
-router.post(
-  "/addComment/:id",
-  [body("comment", "invalid comment").exists().isAlphanumeric()],
-  auth,
-  commentController.addComment
-);
+// router.post(
+//   "/addComment/:id",
+//   [body("comment", "invalid comment").exists().isAlphanumeric()],
+//   auth,
+//   commentController.addComment
+// );
 
-module.exports = router;
+// module.exports = router;
+
+module.exports = (app, io) => {
+  app.post("/addComment", (req, res) => {
+    const comment = req.body.comment;
+
+    console.log("comment");
+
+    io.sockets.emit("comment", comment);
+    res.send({ result: "update sent with IO" });
+  });
+};
